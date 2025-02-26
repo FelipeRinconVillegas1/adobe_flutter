9e8a37e5ae257604464d4d4c26bbb4fd78f422de
dieg0omni <diego.llanten@omni.pro>
2025-02-26 19:12:39 -0300

    Se agrega codigo de example

--- omnipro_ecommerce_sdk/example/lib/main.dart
+++ omnipro_ecommerce_sdk/example/lib/main.dart
@@ -1,23 +1,34 @@
+import 'package:core/domain/entity/categories.dart';
 import 'package:core/domain/entity/omnipro_config/omnipro_configuration.fr.dart';
 import 'package:core/init/setup_omnipro_sdk.dart';
+import 'package:core/utils/init_widget_secure.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:omnipro_ecommerce_sdk/store/home/di/di_categories.fr.dart';
+import 'package:omnipro_ecommerce_sdk/store/home/domain/use_case/get_categoriesby_filter_use_case.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(observers: [], child: MyApp()));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  ConsumerState<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends ConsumerState<MyApp> {
+  late GetCategoriesByFilterUseCase _getCategoriesByFilterUseCase;
+  String response = "";
+
   @override
   void initState() {
-    init();
     super.initState();
+    init();
+    secureInitWidget((_) {
+      _getCategoriesByFilterUseCase = ref.watch(getCategoriesByFilterUseCaseProvider);
+    });
   }
 
   void init() {
@@ -40,10 +51,42 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Plugin example app')),
-        body: Column(
-          children: [
-
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [ElevatedButton(onPressed: ()  async {
+                        final result = await _getCategoriesByFilterUseCase.call(
+                          filter: {
+                            'parent_id': {
+                              'in': ["2"]
+                            }
+                          },
+                          currentPage: 1,
+                        );
+                         result.fold((l) {
+                           print("Error: $l");
+                         }, (categories) {
+                          setState(() {
+                            response = categories.toString();
+                          });
+                        });
+                      }, child: Text("Obtener categorias"))],
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(child: Text(response))
+            ],
+          ),
         ),
       ),
     );
